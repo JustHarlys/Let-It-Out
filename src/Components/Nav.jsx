41977027bf83a07fa9a626b3933c77050a9edf11
harly <harlysmc@gmail.com>
2024-08-22 11:06:16 -0400

    Correción de errores en App.css y los props validation

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import '../App.css'
 
 function Nav({handleWindowsHome, handleWindowsSuggestions, windows, darkMode, toggleDark}) {
--- src/Components/Suggestions.jsx
+++ src/Components/Suggestions.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { nanoid } from 'nanoid';
 import { useState } from 'react';
 
